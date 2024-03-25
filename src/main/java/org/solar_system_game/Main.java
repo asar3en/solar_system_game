14ae769b8cd7907c02c7a064bd1cfa917f9a0e93
asar3en <asar3en@outlook.com>
2024-03-25 09:58:03 +0100

    Add javafx maven plugin and open blank window in main class

--- src/main/java/org/solar_system_game/Main.java
+++ src/main/java/org/solar_system_game/Main.java
@@ -1,7 +1,15 @@
 package org.solar_system_game;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends  Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        mainStage.show();
     }
 }
