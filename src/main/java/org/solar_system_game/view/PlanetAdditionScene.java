ff134f35b12dcabe358eacfb548458b21394770b
Kir-ie <164643984+Kir-ie@users.noreply.github.com>
2024-03-27 12:27:39 +0100

    Added a Mission Creator Scene and a stub for the Planet Addition Scene

---
+++ src/main/java/org/solar_system_game/view/PlanetAdditionScene.java
@@ -0,0 +1,11 @@
+package org.solar_system_game.view;
+
+import javafx.scene.Scene;
+
+public class PlanetAdditionScene implements ViewScene{
+    Scene javaFxScene;
+    public Scene GetJavafxScene() {
+        return javaFxScene;
+    }
+
+}
